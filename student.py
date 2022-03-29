d2eb678b6ef0059f9d1bdb72085c5348e2d6c150
coolbro12321 <100421774+coolbro12321@users.noreply.github.com>
2022-03-29 09:07:37 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,12 +1,12 @@
 def main():
     #your code goes here
     number=float(input('Enter A Number: '))
-    if number%3==0:
-      print("fizz")
+    if number/5%3==0:
+      print("fizzbuzz")
     elif number%5==0:
       print("buzz")
-    elif number%3==0 and number%5==0:
-      print("fizzbuzz")
+    elif number%3==0:
+      print("fizz")
     else:
       print(number)
 
